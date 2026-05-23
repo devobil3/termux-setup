f49c8c51bd528588799605d5b311d7b68d8f5745
devobil3 <devobil321@gmail.com>
2026-05-23 19:29:39 +0700

    Update script.sh v1

--- script.sh
+++ script.sh
@@ -1,1 +1,12 @@
-
+#!/data/data/com.termux/files/usr/bin/bash
+yes | (pkg up && pkg i x11-repo && pkg i termux-x11-nightly proot-distro pulseaudio virglrenderer-android)
+pd i debian
+pd login debian --shared-tmp -- sh -c '
+apt update && DEBIAN_FRONTEND=noninteractive apt install -y sudo chromium xfce4 dbus-x11 locales
+groupadd storage; groupadd wheel; groupadd video
+useradd -mg users -G wheel,audio,video,storage -s /bin/bash nue
+printf "root:000\nnue:000\n" | chpasswd
+echo "nue ALL=(ALL:ALL) ALL" >> /etc/sudoers
+ln -sf /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen && locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf'
