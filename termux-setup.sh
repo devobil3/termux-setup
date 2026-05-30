e703d794ba4fd74509d9d108f0b417d7c25793f7
devobil3 <devobil321@gmail.com>
2026-05-30 07:19:15 -0400

    remove chromium

--- termux-setup.sh
+++ termux-setup.sh
@@ -2,7 +2,7 @@
 yes | (pkg up && pkg i x11-repo && pkg i termux-x11-nightly proot-distro pulseaudio virglrenderer-android)
 pd i debian
 pd login debian --shared-tmp -- sh -c '
-apt update && DEBIAN_FRONTEND=noninteractive apt install -y sudo chromium xfce4 xfce4-terminal dbus-x11 locales fastfetch firefox-esr
+apt update && DEBIAN_FRONTEND=noninteractive apt install -y sudo xfce4 xfce4-terminal dbus-x11 locales fastfetch firefox-esr
 groupadd storage; groupadd wheel; groupadd video
 useradd -mg users -G wheel,audio,video,storage -s /bin/bash nue
 printf "root:000\nnue:000\n" | chpasswd
