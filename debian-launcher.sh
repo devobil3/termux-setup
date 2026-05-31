5eddeb3cf95a8bda7e9c9f423b75727b70612581
devobil3 <devobil321@gmail.com>
2026-05-31 11:07:59 -0400

    Create debian-launcher.sh

---
+++ debian-launcher.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+{
+    pkill -9 -f termux.x11
+    killall -9 pulseaudio virgl_test_server_android
+    export XDG_RUNTIME_DIR=${TMPDIR}
+    termux-x11 :0 -ac & X=$!
+    pulseaudio --start --exit-idle-time=-1
+    pacmd load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1
+    virgl_test_server_android & V=$!
+    proot-distro login debian --user __USERNAME__ --shared-tmp -- bash -c 'export DISPLAY=:0 PULSE_SERVER=tcp:127.0.0.1 GALLIUM_DRIVER=virpipe MESA_GL_VERSION_OVERRIDE=4.0; dbus-launch --exit-with-session startxfce4'
+    kill $X $V 2>/dev/null; pulseaudio --kill
+} >/dev/null 2>&1 &
+
+tput civis; stty -echo
+trap 'tput cnorm; stty echo 2>/dev/null' EXIT
+clr() { printf "\033[3A\r\033[J"; }
+trap 'clr; pkill -9 -f termux.x11 2>/dev/null; killall -9 pulseaudio virgl_test_server_android 2>/dev/null; termux-toast "Aborted."; echo "Aborted."; exit 1' INT
+trap '' TSTP QUIT HUP
+launch() { clr; am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity >/dev/null 2>&1; termux-toast "Debian Launched!"; echo "Debian Launched!"; exit; }
+run_countdown() {
+    local secs=$1
+    local cols=$(tput cols 2>/dev/null || echo 80)
+    local bar_width=$(( cols - 12 ))
+    (( bar_width < 10 )) && bar_width=10
+    local steps=$((secs * 3))
+    printf "Booting Debian...\n[%-${bar_width}s] %ds\n[ENTER] Open now  [CTRL+C] Abort\n" "" "$secs"
+    for i in $(seq 1 $steps); do
+        local filled=$(( i * bar_width / steps ))
+        local bar=""
+        for ((j=0; j<filled; j++)); do bar+="#"; done
+        local t=$(date +%s%3N)
+        printf "\033[2A\r[%-${bar_width}s] %ds\033[K\033[2B\r" "$bar" $(( (steps-i)/3+1 ))
+        while (( $(date +%s%3N) - t < 333 )); do
+            IFS= read -rs -n1 -t0.05 k; rc=$?
+            [[ $rc -eq 0 && -z "$k" ]] && launch
+            while IFS= read -rs -n1 -t0 _ 2>/dev/null; do :; done
+        done
+    done
+    launch
+}
+run_countdown 10
