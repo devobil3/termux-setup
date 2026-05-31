e48dc337dc2dcfdececa1633e27e440657a1dde2
devobil3 <devobil321@gmail.com>
2026-05-31 09:49:27 -0400

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -17,7 +17,7 @@ read -s -p "Enter root password (Press Enter to reuse user password): " r_pw </d
 
 start_time=$(date +%s)
 
-pkg install -y termux-api >/dev/null 2>&1 || true
+pkg install -y termux-api
 
 termux-toast "Starting Debian installation for $user..."
 
